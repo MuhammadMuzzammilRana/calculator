8bc7b0739a783448fb2cc4e2a99637292ad20275
MuhammadMuzzammilRana <mmrr82013@gmail.com>
2026-01-17 14:54:48 +0500

    Implement calculation history in calculator
    Added calculation history feature to the calculator.

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var answer = document.getElementById('screen')
 var user = document.getElementById('input-screen');
 user.innerText = "0"
+var hist = document.getElementById('his-screen')
+var his = []
+var ans = []
 function calculator(){
     var userInput = user.innerText
     answer.innerText = Number(eval(userInput)).toFixed(2)
+    his.push(userInput)
+    ans.push(answer.innerText)
     user.innerText = "0"
 }
 function calc(data){
@@ -25,11 +30,33 @@ function calc(data){
         }
         return
     }
- if (user.innerText === '0') {
+if (data == "his") {
+    if (his.length > 0) {
+        hist.innerHTML = `<h2>Calculation History</h2>`;
+        for (let i = 0; i < his.length; i++) {
+            hist.innerHTML += `<li>${his[i]} = ${ans[i]}</li>`;
+        }
+
+        answer.innerText = "";
+        return;
+    } else {
+        alert('Please first calculate anything.');
+        return;
+    }
+}
+
+    if (data == 'remove-his') {
+        hist.innerHTML = ""
+        return
+    }
+    if (user.innerText === '0') {
         answer.innerText = ""
         user.innerText = data;
+        hist.innerHTML = '';
     } else {
         answer.innerText = ""
         user.innerText += data;
+        hist.innerHTML = '';
     }
 }
+
